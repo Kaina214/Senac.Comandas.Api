7de9706d905fa4d9837c563be43bd04dcc020210
KAINABRUMHABEKOST <kainabrum123@gmail.com>
2025-11-04 21:46:06 -0300

    foi criado o modelBuilder em cardapioitem,Usuario e Mesa

--- Comandas.Api/Comandas.Api/ComandasDBContext.cs
+++ Comandas.Api/Comandas.Api/ComandasDBContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Comandas.Api.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages.Manage;
 
 namespace Comandas.Api
 {
@@ -7,6 +9,56 @@ namespace Comandas.Api
         public ComandasDbContext(DbContextOptions<ComandasDbContext> options)
            : base(options)
         {
+        }
+        //definir algumas configuracoes adicionais no banco 
+        override protected void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Models.Usuario>()
+                .HasData(
+                    
+                new Models.Usuario
+                {
+                    Id = 1,
+                    Nome = "Admin",
+                    Email = "adimin@admin.com",
+                    Senha = "admin123"
+                }
+            );
+
+            modelBuilder.Entity<Mesa>()
+                .HasData(
+
+                new Mesa
+                {
+                    Id = 1,
+                    NumeroMesa = 1,
+                    SituacaoMesa = 0
+                },
+                new Mesa
+                {
+                    Id = 2,
+                    NumeroMesa = 2,
+                    SituacaoMesa = 0
+                },
+                new Mesa
+                {
+                    Id = 3,
+                    NumeroMesa = 3,
+                    SituacaoMesa = 0
+                },
+                modelBuilder.Entity<CardapioItem>()
+                .HasData(
+                    new CardapioItem
+                    {
+                        Id = 1,
+                        Titulo = "Coxinha",
+                        Descricao = "Coxinha de frango com catupiry",
+                        Preco = 6.50M,
+                        PossuiPreparo = true
+                    }
+            ));
+
+            base.OnModelCreating(modelBuilder);
         }
         public DbSet<Models.Usuario> Usuarios { get; set; } = default!;
         public DbSet<Models.Mesa> Mesas { get; set; } = default!;
